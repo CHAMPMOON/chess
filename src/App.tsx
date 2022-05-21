9d01eaa67d18169d9d0b4a26cda13921ed0505c1
CHAMPMOON <antipov8v@gmail.com>
2022-05-21 22:08:41 +0300

    add list lost figures

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import BoardComponent from './components/BoardComponent';
+import LostFigures from './components/LostFigures';
 import { Board } from './models/Board';
 import { Colors } from './models/Colors';
 import { Player } from './models/Player';
@@ -30,12 +31,24 @@ const App = () => {
 
   return (
     <div className='app'>
+      <div>
+        <LostFigures
+          title="Black figures"
+          figures={board.lostBlackFigures}
+        />
+      </div>
       <BoardComponent
         board={board}
         setBoard={setBoard}
         currentPlayer={currentPlayer}
         swapPlayer={swapPlayer}
       />
+      <div>
+        <LostFigures
+          title="White figures"
+          figures={board.lostWhiteFigures}
+        />
+      </div>
     </div>
   );
 }
