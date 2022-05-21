12aed39d8e340d14235171edd654aefdcb8b4c7f
CHAMPMOON <antipov8v@gmail.com>
2022-05-21 14:35:47 +0300

    add logic to move figures

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Board } from "../models/Board";
+import { Cell } from "../models/Cell";
 import CellComponent from "./CellComponent";
 
 interface BoardProps {
@@ -8,14 +9,41 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+    const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+    const click = (cell: Cell) => {
+        if (selectedCell && selectedCell !== cell) {
+            selectedCell.moveFigure(cell);
+            setSelectedCell(null);
+        } else {
+            setSelectedCell(cell);
+        }
+    }
+
+    useEffect(() => {
+        highlightCells();
+    }, [selectedCell])
+
+    const highlightCells = () => {
+        board.highlightCells(selectedCell);
+        updateBoard();
+    }
+
+    const updateBoard = () => {
+        const newBoard = board.getCopyBoard();
+        setBoard(newBoard);
+    }
+
     return (
         <div className="board">
             {board.cells.map((row, index) =>
                 <React.Fragment key={index}>
                     {row.map(cell =>
                         <CellComponent
+                            click={click}
                             cell={cell}
                             key={cell.id}
+                            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
                         />
                     )}
                 </React.Fragment>
